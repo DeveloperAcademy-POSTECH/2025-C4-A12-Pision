e49deddf91b563b916707231572596e2e112aa1c
YeoSeongil <seongil_yeo@naver.com>
2025-07-25 04:24:47 +0900

    [FEAT] 가이딩 카메라 뷰UI 구현

--- Pision/Pision/Source/Feature/Guiding/GuidingCameraView.swift
+++ Pision/Pision/Source/Feature/Guiding/GuidingCameraView.swift
@@ -21,33 +21,61 @@ extension GuidingCameraView {
         .ignoresSafeArea()
       
       VStack {
-        Text("카메라 측정 기준 가이드")
-          .font(.FontSystem.h2)
-          .foregroundStyle(.B_10)
-        
-        VStack(spacing: 29) {
-          ForEach(GuidingCaptionItems.allCases, id: \.self) {
-            GuidingCaptionView(item: $0)
-          }
-        }
-        Spacer()
-        
-        GuidingButtonView(
-          title: "다 음",
-          action: { processNavigation() }
-        )
-        .padding(.bottom, 32)
-        .padding(.horizontal, 21)
+        CustomNavigationbar(title: "측정 가이드")
+        GuideCameraContentView
       }
-      .padding(.top, 55)
-      .frame(maxWidth: 353, maxHeight: 671)
-      .background(.W_00)
-      .clipShape(RoundedRectangle(cornerRadius: 15.27))
     }
     .navigationBarBackButtonHidden()
     .navigationDestination(isPresented: $isNavigate) {
       GuidingPoseView()
     }
+  }
+  
+  private var GuideCameraContentView: some View {
+    VStack {
+      Text("카메라 측정 기준 가이드")
+        .font(.FontSystem.h2)
+        .foregroundStyle(.B_10)
+        .padding(.bottom, 32)
+      
+      Image(.guideP101)
+        .resizable()
+        .frame(width: 124, height: 124)
+        .padding(.bottom, 34)
+      
+      VStack(spacing: 29) {
+        ForEach(GuidingCaptionItems.allCases, id: \.self) {
+          GuidingCaptionView(item: $0)
+        }
+      }
+      .padding(.bottom, 32)
+      
+      HStack(spacing: 32) {
+        Image(.guideP1Sub01)
+          .resizable()
+          .frame(width: 50, height: 50)
+        
+        Image(.guideP1Sub02)
+          .resizable()
+          .frame(width: 50, height: 50)
+        
+        Image(.guideP1Sub03)
+          .resizable()
+          .frame(width: 50, height: 50)
+      }
+      Spacer()
+      
+      GuidingButtonView(
+        title: "다 음",
+        action: { processNavigation() }
+      )
+      .padding(.bottom, 32)
+      .padding(.horizontal, 21)
+    }
+    .padding(.top, 55)
+    .frame(maxWidth: 353, maxHeight: 671)
+    .background(.W_00)
+    .clipShape(RoundedRectangle(cornerRadius: 15.27))
   }
 }
 
