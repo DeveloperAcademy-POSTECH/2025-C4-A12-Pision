90bd65b3e00b4b827b43bac346daee8dc4a1ae74
YeoSeongil <seongil_yeo@naver.com>
2025-07-25 16:50:28 +0900

    [CHORE] 수정

--- Pision/Pision/Source/Feature/Analyze/AnalyzeViewModel.swift
+++
@@ -1,6 +0,0 @@
-//
-//  AnalyzeViewModel.swift
-//  Pision
-//
-//  Created by rundo on 7/24/25.
-//
--- Pision/Pision/Source/Feature/History/HistoryRowView.swift
+++ Pision/Pision/Source/Feature/History/HistoryRowView.swift
@@ -20,49 +20,49 @@ struct HistoryRowView: View {
 extension HistoryRowView {
   var body: some View {
     HStack{
-        VStack(alignment: .leading){
-          HStack{
-            Text("\(formatted(task.startTime))")
-              .font(.FontSystem.b1)
-              .foregroundStyle(.black)
-            Text("~")
-            Text("\(formatted(task.endTime))")
-              .font(.FontSystem.b1)
-              .foregroundStyle(.black)
-            Spacer()
-            Image(systemName: "chevron.right")
-          }
-          //Spacer()
-          Text("")
-          HStack(alignment:.bottom){
-            Text(secondsToHourMinute(task.focusTime))
-              .font(.FontSystem.h1)
-              .foregroundStyle(.BR_00)
-            Text(secondsToHourMinute(task.durationTime))
-              .font(.FontSystem.b1)
-              .foregroundStyle(.B_40)
-            Spacer()
-            Text("\(avgFocus)%")
-              .font(.FontSystem.h2)
-              .foregroundStyle(.black)
-          }
-          CustomProgressBar(value: Double(avgFocus)/100.0)
-          HStack(){
-            RoundedRectangle(cornerRadius: 2)
-              .frame(width: 10, height: 10)
-              .foregroundStyle(.BR_00)
-            Text("집중시간")
-              .font(.FontSystem.btn)
-              .foregroundStyle(.B_40)
-            Spacer()
-            RoundedRectangle(cornerRadius: 2)
-              .frame(width: 10, height: 10)
-              .foregroundStyle(.BR_50)
-            Text("전체시간")
-              .font(.FontSystem.btn)
-              .foregroundStyle(.B_40)
-          }
+      VStack(alignment: .leading){
+        HStack{
+          Text("\(formatted(task.startTime))")
+            .font(.FontSystem.b1)
+            .foregroundStyle(.black)
+          Text("~")
+          Text("\(formatted(task.endTime))")
+            .font(.FontSystem.b1)
+            .foregroundStyle(.black)
+          Spacer()
+          Image(systemName: "chevron.right")
         }
+        //Spacer()
+        Text("")
+        HStack(alignment:.bottom){
+          Text(secondsToHourMinute(task.focusTime))
+            .font(.FontSystem.h1)
+            .foregroundStyle(.BR_00)
+          Text(secondsToHourMinute(task.durationTime))
+            .font(.FontSystem.b1)
+            .foregroundStyle(.B_40)
+          Spacer()
+          Text("\(avgFocus)%")
+            .font(.FontSystem.h2)
+            .foregroundStyle(.black)
+        }
+        CustomProgressBar(value: Double(avgFocus)/100.0)
+        HStack(){
+          RoundedRectangle(cornerRadius: 2)
+            .frame(width: 10, height: 10)
+            .foregroundStyle(.BR_00)
+          Text("집중시간")
+            .font(.FontSystem.btn)
+            .foregroundStyle(.B_40)
+          Spacer()
+          RoundedRectangle(cornerRadius: 2)
+            .frame(width: 10, height: 10)
+            .foregroundStyle(.BR_50)
+          Text("전체시간")
+            .font(.FontSystem.btn)
+            .foregroundStyle(.B_40)
+        }
+      }
     }
     .buttonStyle(PlainButtonStyle())
     .frame(maxWidth:.infinity, maxHeight: .infinity, alignment:.leading)
@@ -73,24 +73,23 @@ extension HistoryRowView {
 }
 
 struct CustomProgressBar: View {
-    var value: Double // 0.0 ~ 1.0
-    var body: some View {
-        GeometryReader { geo in
-            ZStack(alignment: .leading) {
-                Capsule()
-                .foregroundStyle(.BR_50)
-                    .frame(height: 10) // <- 원하는 두께
-                Capsule()
-                .foregroundStyle(.BR_00)
-                    .frame(width: geo.size.width * value, height: 10) // <- 원하는 두께
-            }
-        }
+  var value: Double // 0.0 ~ 1.0
+  var body: some View {
+    GeometryReader { geo in
+      ZStack(alignment: .leading) {
+        Capsule()
+          .foregroundStyle(.BR_50)
+          .frame(height: 10) // <- 원하는 두께
+        Capsule()
+          .foregroundStyle(.BR_00)
+          .frame(width: geo.size.width * value, height: 10) // <- 원하는 두께
+      }
     }
+  }
 }
 
 // MARK: - Func
 extension HistoryRowView{
-  
   func formatted(_ date: Date) -> String {
     let formatter = DateFormatter()
     formatter.dateFormat = "HH:mm"
