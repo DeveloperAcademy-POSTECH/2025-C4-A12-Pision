5b39490572d96379e7d7240c1ca774cb1a5d342b
YeoSeongil <seongil_yeo@naver.com>
2025-07-25 16:51:12 +0900

    [CHORE] 탭바 수정

---
+++ Pision/Pision/Source/Core/Data/Enum/Enum + Tab.swift
@@ -0,0 +1,11 @@
+//
+//  Enum + Tab.swift
+//  Pision
+//
+//  Created by 여성일 on 7/25/25.
+//
+
+enum Tab {
+  case home
+  case record
+}
---
+++ Pision/Pision/Source/Utilities/Components/TabBar/CustomTabBar.swift
@@ -0,0 +1,67 @@
+//
+//  MainTabView.swift
+//  Pision
+//
+//  Created by YONGWON SEO on 7/21/25.
+//
+
+import SwiftUI
+
+struct CustomTabBar: View {
+  @Binding var selectedTab: Tab
+  
+  var body: some View {
+    ZStack {
+      CustomTabBarShape()
+        .fill(Color.white)
+        .frame(height: 90)
+        .shadow(color: .black.opacity(0.1), radius: 6, x: 0, y: -2)
+      
+      HStack {
+        tabItem(icon: "tabHome", label: "홈", tab: .home)
+        Spacer()
+        tabItem(icon: "tabList", label: "기록", tab: .record)
+      }
+      .padding(.horizontal, 48)
+      .padding(.bottom, 16)
+      
+      NavigationLink {
+        MeasureView()
+      } label: {
+        ZStack {
+          Circle()
+            .foregroundStyle(.BR_00)
+            .frame(width: 64, height: 64)
+            .shadow(radius: 4)
+          VStack(spacing: 0) {
+            Image(.tabMeasure)
+              .foregroundColor(.white)
+              
+            Text("측정")
+              .font(.FontSystem.btn)
+              .foregroundColor(.W_00)
+          }
+        }
+      }
+      .offset(y: -32)
+    }
+  }
+  
+  func tabItem(icon: String, label: String, tab: Tab) -> some View {
+    Button {
+      selectedTab = tab
+    } label: {
+      VStack(spacing: 4) {
+        Image(icon)
+          .renderingMode(.template)
+          .font(.FontSystem.cap1)
+          .foregroundColor(selectedTab == tab ? .BR_00 : .B_40)
+        
+        Text(label)
+          .font(.FontSystem.cap1)
+          .foregroundColor(selectedTab == tab ? .BR_00 : .B_40)
+      }
+    }.buttonStyle(.plain)
+  }
+}
+
